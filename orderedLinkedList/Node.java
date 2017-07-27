fe02d57f6153591c3d71c21b937399eca8f4be29
Kalpesh14m <kalpesh.1.mali@gmail.com>
2017-07-27 17:51:17 +0530

    first commit

---
+++ orderedLinkedList/Node.java
@@ -0,0 +1,26 @@
+package orderedLinkedList;
+
+public class Node<E>
+{
+	public String data1;
+	public int data;
+	public Node nextNode;
+	
+	//constructor
+	public Node()
+	{
+		this.nextNode=null;
+	}
+
+	public void setData(String data1)
+	{
+		this.data1=data1;
+		nextNode=null;
+	}
+	
+	public void setData(int data)
+	{
+		this.data=data;
+		nextNode=null;
+	}
+}
